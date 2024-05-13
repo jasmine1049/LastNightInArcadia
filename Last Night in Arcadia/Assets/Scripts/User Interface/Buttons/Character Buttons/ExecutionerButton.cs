b414ed9b797cf920e8371ee4348566e5ded50b2b
Diego <73369316+dnajerab@users.noreply.github.com>
2024-05-13 12:46:01 -0700

    Fixed some silly issues in CharacterButton
    Button set on Awake in CharacterButton (makes sure no null references when Image component is called for Button). ExecutionerButton overrides methods from CharacterButton to make sure no silly shenanigans happen.

--- Last Night in Arcadia/Assets/Scripts/User Interface/Buttons/Character Buttons/ExecutionerButton.cs
+++ Last Night in Arcadia/Assets/Scripts/User Interface/Buttons/Character Buttons/ExecutionerButton.cs
@@ -8,6 +8,18 @@ public class ExecutionerButton : CharacterButton
     public override Character Character { get { return GameManager.Instance.Executioner; } }
 
 
+    public override void Initialize(Character character, GameObject objectToDeactivateOnClick, GameObject objectToActivateOnClick)
+    {
+        return;
+    }
+
+
+    public override void OnClick()
+    {
+        return;
+    }
+
+
     public override void UpdateUI()
     {
         return;
